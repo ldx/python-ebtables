0dd4c2776f210ff2bed569a9a4a881e14bdb684b
Vilmos Nebehaj <v.nebehaj@gmail.com>
2015-01-10 01:25:25 +0100

    Generate rst README for pypi.

--- setup.py
+++ setup.py
@@ -1,9 +1,36 @@
-# from distutils.core import setup
+import os
+import subprocess
 from setuptools import setup
 
 """python-ebtables setup script"""
 
 import ebtables
+
+long_description = """Ebtables is used for Ethernet bridge frame table
+administration on Linux. Python-ebtables is a simple Python binding for
+Ebtables."""
+
+try:
+    create_rst = True
+    if os.path.exists('README.rst'):
+        s1 = os.stat('README.md')
+        s2 = os.stat('README.rst')
+        if s1.st_mtime <= s2.st_mtime:
+            create_rst = False
+    if create_rst:
+        rc = subprocess.call(['pandoc',
+                              '-o', 'README.rst',
+                              '-f', 'markdown',
+                              '-t', 'rst',
+                              'README.md'],
+                             shell=False,
+                             stdout=subprocess.PIPE)
+        if rc == 0:
+            with open('README.rst') as f:
+                long_description = f.read()
+except:
+    pass
+
 
 setup(
     name='ebtables',
@@ -12,6 +39,7 @@ setup(
     author_email='v.nebehaj@gmail.com',
     url='https://github.com/ldx/python-ebtables',
     description='Linux Ebtables bindings',
+    long_description=long_description,
     py_modules=['ebtables'],
     setup_requires=['cffi'],
     install_requires=['cffi'],
